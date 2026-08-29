b50f2de545f5397d271e77d6066fa469a498a3c7
guybarnahum <guybarnahum@users.noreply.github.com>
2026-08-29 09:12:56 -0700

    Use root Infra seam for credentialed generation

--- services/asset-generator/src/credentialed-asset-generation.mjs
+++ services/asset-generator/src/credentialed-asset-generation.mjs
@@ -1,4 +1,4 @@
-import { InfraImmutableObjectConflictError } from "@fibre/infra";
+import { InfraImmutableObjectConflictError } from "#infra";
 import { normalizeAssetGenerationJob } from "./asset-generation-domain.mjs";
 import { assetGenerationJobDigest } from "./asset-generation-attempt.mjs";
 import { toAssetGenerationError } from "./asset-generation-error.mjs";
