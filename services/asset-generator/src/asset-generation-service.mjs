594866bff2ac1b7252c9d86792ce592166c41977
guybarnahum <guybarnahum@users.noreply.github.com>
2026-08-29 09:05:16 -0700

    Use root Infra seam for asset service

--- services/asset-generator/src/asset-generation-service.mjs
+++ services/asset-generator/src/asset-generation-service.mjs
@@ -1,4 +1,4 @@
-import { requireInfraCapabilities } from "../../../packages/infra/src/infra-driver.mjs";
+import { requireInfraCapabilities } from "#infra";
 import {
   ASSET_GENERATION_RECEIPT_VERSION,
   assertMediaGenerationProvider,
