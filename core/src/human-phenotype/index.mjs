f6a0c885897b7201a8c23d0f768ce37f41af15da
guybarnahum <guybarnahum@users.noreply.github.com>
2026-09-26 10:06:32 -0700

    Export unified birth inheritance

--- core/src/human-phenotype/index.mjs
+++ core/src/human-phenotype/index.mjs
@@ -3,3 +3,4 @@ export {phenotypeFromPhysicalGenome} from "./phenotype.mjs";
 export {sampleFamilyAncestry} from "./population-ancestry.mjs";
 export {createPhysicalGenome, recombinePhysicalGenomes, expressPhysicalGenome, physicalGenomeLoci} from "./physical-genome.mjs";
 export {sampleFounderPhysicalGenome} from "./founder-genome.mjs";
+export {resolveBirthPhysicalInheritance} from "./birth-inheritance.mjs";
