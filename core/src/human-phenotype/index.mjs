466e1c9a3cd6981cfa377309b629067fd397912a
guybarnahum <guybarnahum@users.noreply.github.com>
2026-09-26 08:46:26 -0700

    Expose genome-only phenotype expression

--- core/src/human-phenotype/index.mjs
+++ core/src/human-phenotype/index.mjs
@@ -1,5 +1,5 @@
 export {normalizeAncestry, inheritedAncestry} from "./ancestry.mjs";
-export {sampleInheritedPhenotype} from "./phenotype.mjs";
+export {phenotypeFromPhysicalGenome} from "./phenotype.mjs";
 export {sampleFamilyAncestry} from "./population-ancestry.mjs";
 export {createPhysicalGenome, recombinePhysicalGenomes, expressPhysicalGenome, physicalGenomeLoci} from "./physical-genome.mjs";
 export {sampleFounderPhysicalGenome} from "./founder-genome.mjs";
