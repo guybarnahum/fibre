7445bdbed0ad0fc4eddab1983237e7ca406c8676
guybarnahum <guybarnahum@users.noreply.github.com>
2026-09-26 08:45:03 -0700

    Export founder genome sampler

--- core/src/human-phenotype/index.mjs
+++ core/src/human-phenotype/index.mjs
@@ -2,3 +2,4 @@ export {normalizeAncestry, inheritedAncestry} from "./ancestry.mjs";
 export {sampleInheritedPhenotype} from "./phenotype.mjs";
 export {sampleFamilyAncestry} from "./population-ancestry.mjs";
 export {createPhysicalGenome, recombinePhysicalGenomes, expressPhysicalGenome, physicalGenomeLoci} from "./physical-genome.mjs";
+export {sampleFounderPhysicalGenome} from "./founder-genome.mjs";
