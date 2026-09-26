f52843366f4384dc631896b5f22ea496e6080d02
guybarnahum <guybarnahum@users.noreply.github.com>
2026-09-26 00:10:38 -0700

    Export shared physical genome core

--- core/src/human-phenotype/index.mjs
+++ core/src/human-phenotype/index.mjs
@@ -1,3 +1,4 @@
 export {normalizeAncestry, inheritedAncestry} from "./ancestry.mjs";
 export {sampleInheritedPhenotype} from "./phenotype.mjs";
 export {sampleFamilyAncestry} from "./population-ancestry.mjs";
+export {createPhysicalGenome, recombinePhysicalGenomes, expressPhysicalGenome, physicalGenomeLoci} from "./physical-genome.mjs";
